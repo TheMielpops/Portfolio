940aa2649bf337290fbc4546f1618805da2dd5ce
Noah Dauge <noah05dauge@gmail.com>
2025-04-10 18:04:30 +0200

    correction du eval()

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ["lucide-react"],
+  },
+  server: {
+    hmr: false, // Désactive le hot module replacement en mode développement
   },
 });
+/* 
+  Content-Security-Policy: script-src 'self' 'unsafe-eval' 
+*/
